0cddd4c1d369925a2d1504b0fc0043efd9bbcb01
Ashlyn Knox <ashlyn@lilyx.io>
2023-12-12 17:07:17 -0700

    initial commit
    Signed-off-by: Ashlyn Knox <ashlyn@lilyx.io>

---
+++ data/finals-w23.js
@@ -0,0 +1,21 @@
+const round1 = [
+  {
+    name: 'Caku J',
+    title: 'FTCC Fellowship'
+  },
+  {
+    name: 'Sidney Lemon',
+    title: 'Pizza.com'
+  },
+  {
+    name: 'Rashid H',
+    title: 'Barbershop Website'
+  },
+  {
+    name: 'Cordelle',
+    title: 'Web 3 App'
+  }
+];
+
+
+export { round1 };
