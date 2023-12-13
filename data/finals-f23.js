9f05403b669297985a7b3de934c656bf06254661
Ashlyn Knox <ashlyn@lilyx.io>
2023-12-12 20:43:10 -0700

    roster and presentation data

---
+++ data/finals-f23.js
@@ -0,0 +1,67 @@
+const roundOne = [
+  {
+    name: "Brendan Malycky",
+    title: "Pokellect",
+  },
+  {
+    name: "Chad Cervantes",
+    title: "Common Ground Dance",
+  },
+  {
+    name: "Edward Mah",
+    title: "Donut Train",
+  },
+  {
+    name: "Cal Holt",
+    title: "Crimp",
+  },
+  {
+    name: "Jessica Nelson",
+    title: "Out of this World Portfolio",
+  },
+  {
+    name: "Ashkie Esperanza",
+    title: "Recreation of Pacman",
+  },
+  {
+    name: "Reilly Wooldridge",
+    title: "2D Endless Runner",
+  },
+];
+
+const roundTwo = [
+  {
+    name: "Sterling Smith",
+    title: "Art Portfolio",
+  },
+  {
+    name: "Dorothy Nguyen",
+    title: "Checkout",
+  },
+  {
+    name: "Emily Seafoot",
+    title: "The Paperback Post Journal",
+  },
+  {
+    name: "Kevin Fula",
+    title: "Marvel Timeline Gallery",
+  },
+  {
+    name: "Robbie Soriano",
+    title: "The Dragon Sanctuary",
+  },
+  {
+    name: "Akash Samra",
+    title: "Portfolio Site",
+  },
+  {
+    name: "Sydney Bruce",
+    title: "Hot Wax Music",
+  },
+  {
+    name: "John Dagsa",
+    title: "Portfolio Site",
+  },
+];
+
+export { roundOne, roundTwo };
---
+++ data/roster-f23.js
@@ -0,0 +1,239 @@
+export const roster = [
+  {
+    label: "Brendan Malycky",
+    tagline: "Just a guy learning to code",
+    avatar: "brendan.jpg",
+    links: [
+      {
+        label: "github",
+        link: "https://github.com/brendanm403",
+      },
+    ],
+    badges: [],
+  },
+  {
+    label: "Chad Cervantes",
+    tagline: "Junior Web Developer student at SAIT",
+    avatar: "",
+    links: [
+      {
+        label: "github",
+        link: "https://www.github.com/chad-cervantes",
+      },
+      {
+        label: "linkedin",
+        link: "https://www.linkedin.com/in/chadcervantes",
+      },
+    ],
+    badges: [],
+  },
+  {
+    label: "Edward Mah",
+    tagline: "Frontend Developer focused on 3D interatctive sites",
+    avatar: "",
+    links: [
+      {
+        label: "github",
+        link: "https://www.github.com/edwardm8",
+      },
+      {
+        label: "linkedin",
+        link: "https://www.linkedin.com/in/edward-mah-7a67b2239",
+      },
+    ],
+    badges: [],
+  },
+  {
+    label: "Cal Holt",
+    tagline: "Junior Web Developer",
+    avatar: "cal.jpg",
+    links: [
+      {
+        label: "github",
+        link: "https://github.com/CalHolt",
+      },
+      {
+        label: "email",
+        link: "mailto:calumholt@hotmail.com",
+      },
+    ],
+    badges: [],
+  },
+  {
+    label: "Jessica Nelson",
+    tagline: "",
+    avatar: "jessica.jpg",
+    links: [
+      {
+        label: "github",
+        link: "https://www.github.com/jessicamnelsonn",
+      },
+      {
+        label: "linkedin",
+        link: "https://www.linkedin.com/in/jessicamnelson",
+      },
+    ],
+    badges: [],
+  },
+  {
+    label: "Ashkie Esperanza",
+    tagline: "Junior Indie Game Developer",
+    avatar: "",
+    links: [
+      {
+        label: "github",
+        link: "https://github.com/AshkieCharles)",
+      },
+      {
+        label: "email",
+        link: "mailto:Ashkiecharles@gmail.com",
+      },
+      {
+        label: "linkedin",
+        link: "https://www.linkedin.com/in/ashkie-esperanza-77255926a/",
+      },
+      {
+        label: "phone",
+        link: "403-477-5657",
+      },
+    ],
+    badges: [],
+  },
+  {
+    label: "Reilly Wooldridge",
+    tagline: "Graphic Designer, Cartoonist, Coder",
+    avatar: "reilly.jpg",
+    links: [
+      {
+        label: "github",
+        link: "https://github.com/HazyInk",
+      },
+      {
+        label: "linkedin",
+        link: "https://www.linkedin.com/in/reilly-shea-paston-4b6a22264/",
+      },
+      {
+        label: "portfolio",
+        link: "https://reillysheapaston.myportfolio.com/",
+      },
+    ],
+    badges: [],
+  },
+  {
+    label: "Sterling Smith",
+    tagline:
+      "Junior Web Developer and abstract artist ready to slide from the ski industry into the tech world!",
+    avatar: "",
+    links: [
+      {
+        label: "github",
+        link: "https://github.com/AbstractSter",
+      },
+      {
+        label: "email",
+        link: "mailto:sterling12311@gmail.com",
+      },
+      {
+        label: "linkedin",
+        link: "https://www.linkedin.com/in/sterlingsmithyyc/",
+      },
+      {
+        label: "instagram",
+        link: "https://www.instagram.com/sterlingsmithyyc",
+      },
+      {
+        label: "tiktok",
+        link: "https://www.tiktok.com/@sterlingartstudio",
+      },
+    ],
+    badges: [],
+  },
+  {
+    label: "Dorothy Ngyuen",
+    tagline: "",
+    avatar: "",
+    links: [
+      {
+        label: "github",
+        link: "",
+      },
+    ],
+    badges: [],
+  },
+  {
+    label: "Emily Seafoot",
+    tagline: "Fullstack Web Developer",
+    avatar: "emily.jpg",
+    links: [
+      {
+        label: "github",
+        link: "https://github.com/Emilypearl91",
+      },
+    ],
+    badges: [],
+  },
+  {
+    label: "Kevin Fula",
+    tagline: "Front End Web Developer",
+    avatar: "kevin.jpg",
+    links: [
+      {
+        label: "github",
+        link: "https://github.com/Raptorshere",
+      },
+    ],
+    badges: [],
+  },
+  {
+    label: "Robbie Soriano",
+    tagline: "",
+    avatar: "",
+    links: [
+      {
+        label: "github",
+        link: "",
+      },
+    ],
+    badges: [],
+  },
+  {
+    label: "Akash Samra",
+    tagline: "",
+    avatar: "",
+    links: [
+      {
+        label: "github",
+        link: "",
+      },
+    ],
+    badges: [],
+  },
+  {
+    label: "Sydney Bruce",
+    tagline: "Let's make it happen",
+    avatar: "",
+    links: [
+      {
+        label: "github",
+        link: "https://github.com/sydneyYYC",
+      },
+    ],
+    badges: [],
+  },
+  {
+    label: "John Dagsa",
+    tagline: "Web Developer",
+    avatar: "john.jpg",
+    links: [
+      {
+        label: "github",
+        link: "https://github.com/jdvgsa",
+      },
+      {
+        label: "linkedin",
+        link: "https://github.com/jdvgsa",
+      },
+    ],
+    badges: [],
+  },
+];
