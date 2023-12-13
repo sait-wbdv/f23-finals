03678706190ac85046ffec7d8d84820866d8e88c
Ashlyn Knox <ashlyn@lilyx.io>
2023-12-12 23:10:08 -0700

    component prop edit and error handling

--- components/Presentation.vue
+++ components/Presentation.vue
@@ -1,32 +1,38 @@
 <script setup>
-import { roster } from '../data/roster-w23';
-
+import { roster } from "../data/roster-f23";
 const props = defineProps({
-  round: {type: Object},
-  index: {type: Number}
+  round: { type: Object },
+  index: { type: Number },
 });
 </script>
 
 <template>
-  <h3 class="rounds open">Presentations: {{round.start}}</h3>
+  <h3 class="rounds open">Presentations: {{ round.start }}</h3>
   <!-- <h3 class="rounds open">{{round.start}}: Round {{(index + 1)}} Presentations</h3> -->
-      <section :id="round.container">
-        <article class="presentation" v-for="speaker in round.speakers" :key="speaker.id" :speaker=speaker>
-          <header><h4>{{speaker.title}}</h4></header>
-          <div>
-            <ul>
-              <li v-if="!speaker.members">
-                <RosterCard :student="roster.find(item => item.label === speaker.name)">
-                  <slot></slot>
-                </RosterCard>
-              </li>
-              <li v-else v-for="student in speaker.members" :key="student.id">
-                <RosterCard :student="roster.find(item => item.label === student)">
-                  <slot></slot>
-                </RosterCard>
-              </li>
-            </ul>
-          </div>
-        </article>
-      </section>
+  <section :id="round.container">
+    <article
+      class="presentation"
+      v-for="speaker in round.speakers"
+      :key="speaker.id"
+      :speaker="speaker"
+    >
+      <header>
+        <h4>{{ speaker.title }}</h4>
+      </header>
+      <div>
+        <ul>
+          <li v-if="!speaker.members">
+            <RosterCard
+              :student="roster.find((item) => item.label === speaker.name)"
+            />
+          </li>
+          <li v-else v-for="student in speaker.members" :key="student.id">
+            <RosterCard
+              :student="roster.find((item) => item.label === student)"
+            />
+          </li>
+        </ul>
+      </div>
+    </article>
+  </section>
 </template>
--- components/RosterCard.vue
+++ components/RosterCard.vue
@@ -1,25 +1,44 @@
 <script setup>
-const props = defineProps({student: {type: Object}});
+const props = defineProps({
+  student: {
+    type: Object,
+    default() {
+      return {
+        avatar: "default.svg",
+        label: "default label",
+        tagline: "",
+      };
+    },
+  },
+});
+console.log(props);
 </script>
 
 <template>
   <figure>
-    <img :src="'/images/roster/w23/' + student.avatar" :alt="student.label + ' Profile Pic'">
+    <img
+      :src="'/images/roster/f23/' + student.avatar"
+      :alt="student.label + ' Profile Pic'"
+    />
     <figcaption>
       <aside class="badges">
-        <ul>
-          <Badge v-for="badge in student.badges" :key="badge.id" :badge=badge>
-            <slot></slot>
-          </Badge>
-        </ul>
+        <!-- <ul>
+          <Badge
+            v-for="badge in student.badges"
+            :key="badge.id"
+            :badge="badge"
+          />
+        </ul> -->
       </aside>
-      <header>{{student.label}}</header>
-      <p>{{student.tagline}}</p>
+      <header>{{ student.label }}</header>
+      <p>{{ student.tagline }}</p>
       <footer class="social">
         <ul>
-          <SocialLink v-for="link in student.links" :key="link.id" :link=link>
-              <slot></slot>
-          </SocialLink>
+          <SocialLink
+            v-for="link in student.links"
+            :key="link.id"
+            :link="link"
+          />
         </ul>
       </footer>
     </figcaption>
